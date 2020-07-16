baac593c75d1fd03d991070b4e676d941dc1009b
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-07-16 22:10:43 +0900

    Create myBuySpec.php

---
+++ secondHand/myBuySpec.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="ko">
+<?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/head.php';?>
+<body>
+<div class="container">
+	<?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/header.php';?>
+
+
+	<?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/top.php';?>
+
+
+	<div class="contents">
+       
+
+        <div class="userCardGroup">
+            <div class="userCardBox">
+                <div class="userImgBox">
+                    <!-- <img> -->
+                </div>
+                <div class="nameTitle">닉네임</div>
+                <div class="ratingGroup">
+					<a href="javascript:void(0);" class="rStarIcon activated"></a>
+					<a href="javascript:void(0);" class="rStarIcon activated"></a>
+					<a href="javascript:void(0);" class="rStarIcon activated"></a>
+					<a href="javascript:void(0);" class="rStarIcon activated"></a>
+					<a href="javascript:void(0);" class="rStarIcon activated"></a>
+				</div>
+                <div>
+                    <a href="javascript:Void(0);" class="bdBtn">내 상점 관리</a>
+                </div>
+            </div>
+
+
+            <div class="specInfoBox">
+                <div class="upperGroup">
+                    <div class="titleBox">닉네임</div>
+                    <div class="infoBox">6개월간 판매 11건 (거래종료 9건   판매취소 0건    구매취소 0건    반품 2건) </div>
+                </div>
+                <div class="lowerGroup">
+                    <div class="infoBox">
+                        정상해지 된 공기계 판매합니다.</br>
+                        분실/도난/침수폰은 절대 판매하지 않습니다.</br>
+                        안전하게 구매해주세요^^ 
+                    </div>
+                    <a href="javascript:void(0);" class="bdBtn">소개글 수정</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="moveToPGtab sndMySort">
+            <a href="javascript:void(0);" class="tOnlyBtn">판매물품</a>
+            <a href="javascript:void(0);" class="tOnlyBtn">구매물품</a>
+            <a href="javascript:void(0);" class="tOnlyBtn activated">받은 문의</a>
+            <a href="javascript:void(0);" class="tOnlyBtn">판매자후기관리</a>
+            <a href="javascript:void(0);" class="tOnlyBtn">고객센터 문의</a>
+        </div>
+
+        <div class="mainCon listSort">
+
+
+
+            <!-- <div class="lfFloat">
+                <div class="titleBox">판매물품</div>
+                <ul class="menuGroup commSort">
+                    <li>
+                        <a href="javascript:void(0);" class="tOnlyBtn activated">전체 판매물품</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);" class="tOnlyBtn ">판매완료 물품</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);" class="tOnlyBtn ">취소/환불/반품</a>
+                    </li>
+                </ul>
+            </div> -->
+
+
+
+
+
+		    <div class="section commSort">
+
+                <div class="listTopGroup schSort periodSort">
+
+                    <div class="monthSelGroup">
+                        <a href="javascript:void(0);" class="bgBtn activated">전체</a>
+                        <a href="javascript:void(0);" class="bgBtn">1개월</a>
+                        <a href="javascript:void(0);" class="bgBtn">3개월</a>
+                        <a href="javascript:void(0);" class="bgBtn">6개월</a>
+                        <a href="javascript:void(0);" class="bgBtn">12개월</a>
+                    </div>
+                    <div class="calendarGroup">
+                        <div class="datePickGroup">
+                            <input class="tbox dateBox">
+                            <i class="calendarIcon"></i>
+                        </div>
+                        <span>~</span>
+                        <div class="datePickGroup">
+                            <input class="tbox dateBox">
+                            <i class="calendarIcon"></i>
+                        </div>
+                        <a class="bgBtn searchSort">조회</a>
+                    </div>
+                </div>
+
+                <!-- <div class="listTopCounter">
+                    <span class="titleBox">전체</span>
+                    <span class="countNum">6개</span>
+                </div> -->
+
+
+
+
+
+                <ul class="toggleListGroup">
+                    <li class="fstLayer">
+                        <div class="liMConGroup">
+                            <img src="/sellPhone/common/img/purchImg_1.png" class="purchImg">
+                            <div class="listDateGroup">
+                                <span class="userNameBox">redme**</span>
+                                <span class="listDateBox">2020.05.01</span>
+                            </div>
+                            <div class="infoGroup">
+                                <span>애플 아이폰 8 공기계 중고폰 3사호환 무약정</span>
+                                <span class="bulkHead"></span>
+                                <span>용량:256GB / 색상:실버 / 등급:A급</span>
+                            </div>
+                            <a href="javascript:void(0);" class="bdBtn">자세히보기</a>
+                        </div>
+
+
+
+                        <div class="liTogGroup">
+                            <pre>
+안녕하세요?
+Galaxy S9 PLUS (256GB) 30대
+Galaxy S10 5G (256GB) 20대
+A,B급이면 되구요, 납기는 4월 중순까지면 됩니다.
+
+견적 부탁드려요.
+
+향후 계속 추가 구매 상황이 생길 것 같습니다.
+우선 초도 물량으로 요정도만 견적 요청합니다.
+
+물건 수령시 직접 방문 드릴 예정입니다.
+
+제 전화번호는 010 3357 0276 입니다.
+                            </pre>
+                            <a href="javascript:void(0);" class="bdBtn">접기</a>
+                        </div>
+
+
+                        <ul class="liCommGroup">
+                            <li>
+                                <span>comments</span>
+                                <span>메일주소는 abc@naver.com 입니다.</span>
+                            </li>
+                            <li>
+                                <input>
+                                <a href="javascript:void(0);" class="bgBtn">등록</a>
+                            </li>
+                        </ul>
+
+
+                    </li>
+
+
+
+                </ul>
+
+
+
+
+
+
+
+
+
+
+
+                
+
+                <!-- <div class="btnGroup moreShowSort">
+                    <a href="javascript:void(0);" class="tOnlyBtn">더보기</a>
+                </div> -->
+            </div>
+        </div>
+        <a href="javascript:Void(0);" class="icBtn rtFloat"></a>
+    </div>
+
+
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/bottom.php';?>
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/footer.php';?>
+
+
+</div>
+
+
+<!-- <script src="/intro/js/join.js"></script> -->
+
+
+</body>
+</html>
