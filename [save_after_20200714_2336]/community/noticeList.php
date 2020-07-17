546386256042e97633267854c58903514633f09d
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-07-17 14:53:20 +0900

    Add files via upload

---
+++ [save_after_20200714_2336]/community/noticeList.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="ko">
+<?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/head.php';?>
+<body>
+<div class="container">
+	<?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/header.php';?>
+
+
+	<?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/top.php';?>
+
+
+	<div class="contents">
+       
+        <div class="topSearchGroup">
+            <span class="txtLiner">내 폰 팔면 얼마 받을 수 있나요?</span>
+            <div class="sboxGroup">
+                <select class="sbox nomSort">
+					<option>전체</option>
+					<option>과목명</option>
+					<option>적요명</option>
+                </select>
+                <select class="sbox nomSort">
+					<option>전체</option>
+					<option>과목명</option>
+					<option>적요명</option>
+                </select>
+            </div>
+            <span class="txtLiner">S급 매입시세</span>
+            <span class="txtLiner priceSort">10,000원</span>
+            <a href="javascript:void(0);" class="bgBtn nomSort">판매 신청하기</a>
+        </div>
+        <div class="bannerGroup pgTopSort">
+            <a href="javascript:void(0);" class="bannerBtn">
+                <img src="/sellPhone/common/img/communityBanner_1.png">
+            </a>
+        </div>
+        <div class="moveToPGtab">
+            <a href="javascript:void(0);" class="tOnlyBtn activated">공지사항</a>
+            <a href="javascript:void(0);" class="tOnlyBtn">언론칭찬</a>
+            <a href="javascript:void(0);" class="tOnlyBtn">자주묻는질문</a>
+            <a href="javascript:void(0);" class="tOnlyBtn ">이용후기</a>
+        </div>
+
+        <div class="mainCon listSort">
+
+
+
+            <!-- <div class="lfFloat">
+                <div class="titleBox">판매물품</div>
+                <ul class="menuGroup commSort">
+                    <li>
+                        <a href="javascript:void(0);" class="tOnlyBtn activated">전체 판매물품</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);" class="tOnlyBtn ">판매완료 물품</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);" class="tOnlyBtn ">취소/환불/반품</a>
+                    </li>
+                </ul>
+            </div> -->
+
+
+            <div class="lfFloat">
+                <div class="titleBox">이용후기</div>
+                <ul class="menuGroup commSort">
+                    <li>
+                        <a href="javascript:void(0);" class="tOnlyBtn activated">전체</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);" class="tOnlyBtn ">내폰 매매 후기</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);" class="tOnlyBtn ">내폰 구입 후기</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);" class="tOnlyBtn ">내폰 수리 후기</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);" class="tOnlyBtn ">엔지니어 직접후기</a>
+                    </li>
+                </ul>
+            </div>
+
+
+
+
+		    <div class="rtSection commSort">
+
+                <!-- <div class="listTopGroup schSort periodSort">
+                    <div class="monthSelGroup">
+                        <a href="javascript:void(0);" class="bgBtn activated">전체</a>
+                        <a href="javascript:void(0);" class="bgBtn">1개월</a>
+                        <a href="javascript:void(0);" class="bgBtn">3개월</a>
+                        <a href="javascript:void(0);" class="bgBtn">6개월</a>
+                        <a href="javascript:void(0);" class="bgBtn">12개월</a>
+                    </div>
+                    <div class="calendarGroup">
+                        <div class="datePickGroup">
+                            <input class="tbox dateBox">
+                            <i class="calendarIcon"></i>
+                        </div>
+                        <span>~</span>
+                        <div class="datePickGroup">
+                            <input class="tbox dateBox">
+                            <i class="calendarIcon"></i>
+                        </div>
+                        <a class="bgBtn searchSort">조회</a>
+                    </div>
+                </div> -->
+
+                <!-- <div class="listTopCounter">
+                    <span class="titleBox">전체</span>
+                    <span class="countNum">6개</span>
+                </div> -->
+
+                <div class="listTopGroup schSort">
+                    <div class="tboxGroup">
+                        <input class="tbox" placeholder="검색어를 입력하세요.">
+                        <a href="javascript:Void(0);" class="iconBtn searchSort"></a>
+                    </div>
+                </div>
+
+
+
+                <ul class="toggleListGroup">
+                    <li class="fstLayer">
+                        <div class="liMConGroup">
+                            <img src="/sellPhone/common/img/purchImg_1.png" class="purchImg">
+                            <div class="listDateGroup">
+                                <span class="userNameBox">redme**</span>
+                                <span class="listDateBox">2020.05.01</span>
+                            </div>
+                            <div class="infoGroup">
+                                <span>애플 아이폰 8 공기계 중고폰 3사호환 무약정</span>
+                                <span class="bulkHead"></span>
+                                <span>용량:256GB / 색상:실버 / 등급:A급</span>
+                            </div>
+                            <a href="javascript:void(0);" class="bdBtn">자세히보기</a>
+                        </div>
+                        <div class="liTogGroup">
+                            <pre>
+안녕하세요?
+Galaxy S9 PLUS (256GB) 30대
+Galaxy S10 5G (256GB) 20대
+A,B급이면 되구요, 납기는 4월 중순까지면 됩니다.
+
+견적 부탁드려요.
+
+향후 계속 추가 구매 상황이 생길 것 같습니다.
+우선 초도 물량으로 요정도만 견적 요청합니다.
+
+물건 수령시 직접 방문 드릴 예정입니다.
+
+제 전화번호는 010 3357 0276 입니다.
+                            </pre>
+                            <a href="javascript:void(0);" class="bdBtn">접기</a>
+                        </div>
+
+
+                        <div class="commentGroup inListSort">
+                            <div class="commMainInfo">
+                                <div class="commDivision">comments</div>
+                                <div class="commInfoLiner">메일주소는 abc@naver.com 입니다.</div>
+                            </div>
+                            <ul class="commAddGroup">
+                                <li>
+                                    <div class="tBoxGroup btnMixSort">
+                                        <input class="tbox" placeholder="댓글을 입력해주세요.">
+                                        <a href="javascript:void(0);" class="bgBtn">등록</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+
+
+                <div class="btnGroup moreShowSort">
+                    <a href="javascript:void(0);" class="tOnlyBtn">더보기</a>
+                </div>
+            </div>
+        </div>
+        <a href="javascript:Void(0);" class="icBtn rtFloat"></a>
+    </div>
+
+
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/bottom.php';?>
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/footer.php';?>
+
+
+</div>
+
+
+<!-- <script src="/intro/js/join.js"></script> -->
+
+
+</body>
+</html>
