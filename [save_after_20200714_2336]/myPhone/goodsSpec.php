d8ad08b6c146d900412ef7426f364ba0596596bc
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-07-24 21:18:09 +0900

    Create goodsSpec.php

---
+++ [save_after_20200714_2336]/myPhone/goodsSpec.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="ko">
+<?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/head.php';?>
+<body>
+<div class="container">
+	<?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/header.php';?>
+
+
+	<?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/top.php';?>
+
+
+	<div class="contents">
+
+
+        <div class="topSearchGroup">
+            <span class="txtLiner">내 폰 팔면 얼마 받을 수 있나요?</span>
+            <div class="sboxGroup">
+                <select class="sbox nomSort">
+					<option>전체</option>
+					<option>과목명</option>
+					<option>적요명</option>
+                </select>
+                <select class="sbox nomSort">
+					<option>전체</option>
+					<option>과목명</option>
+					<option>적요명</option>
+                </select>
+            </div>
+            <span class="txtLiner">S급 매입시세</span>
+            <span class="txtLiner priceSort">10,000원</span>
+            <a href="javascript:void(0);" class="bgBtn nomSort">판매 신청하기</a>
+        </div>
+
+
+
+        <!-- <div class="bannerGroup pgTopSort"> -->
+        <div class="bannerGroup myPhSort">
+            <a href="javascript:void(0);" class="bannerBtn">
+                <img src="/sellPhone/common/img/sellMyPhBanner_1.png">
+            </a>
+        </div>
+
+
+        <!-- <div class="openerGroup listOpenSort">
+            <a href="javascript:void(0);" class="tOnlyBtn">물품보기</a>
+        </div> -->
+
+
+
+
+
+        <!-- <div class="pgTopTitGroup">
+            <div class="mainTitle">내 폰 팔기</div>
+            <div class="subTitle">가장 높은 매입가격으로 판매!</div>
+        </div> -->
+
+
+
+
+        <div class="bannerGroup gdSaleRegGroup">
+
+            <div class="pgRouteBox">홈  /  내폰 팔기  /  아이폰XS 맥스</div>
+
+
+
+            <!-- <div class="temporalBannerArea">
+                배너구역
+            </div> -->
+            <div class="saleGdImgBox">
+                <img src="/sellPhone/common/img/saleGoodsImg_1.png">
+            </div>
+
+
+
+
+            <div class="saleGdRadioGroup">
+                <div class="">
+
+                    <div class="titleBox">아이폰XS 맥스</div>
+                    <div>
+                        <label>
+
+                        </label>
+                    </div>
+
+                </div>
+                    
+            </div>
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- <ul class="imgFaqGroup"> -->
+            <!-- <li class="faqBox"> -->
+                <!-- <img src="/sellPhone/common/img/imgFaq_1.png" class="backImg"> -->
+                <!-- <div class="infoGroup"> -->
+                    <!-- <div class="titleBox">거래방법이</br>궁금하신가요?</div> -->
+                    <!-- <a href="javascirpt:void(0);" class="tOnlyBtn toSpecSort">자세히보기</a> -->
+                    <!-- <img src="/sellPhone/common/img/underBtnLiner_1.png" class="underBtn"> -->
+                <!-- </div>  -->
+            <!-- </li> -->
+            <!-- <li class="faqBox"> -->
+                <!-- <img src="/sellPhone/common/img/imgFaq_2.png" class="backImg"> -->
+                <!-- <div class="infoGroup"> -->
+                    <!-- <div class="titleBox">거래하려면</br>얼마나 걸리나요?</div> -->
+                    <!-- <a href="javascirpt:void(0);" class="tOnlyBtn toSpecSort">자세히보기</a> -->
+                    <!-- <img src="/sellPhone/common/img/underBtnLiner_1.png" class="underBtn"> -->
+                <!-- </div>  -->
+            <!-- </li> -->
+            <!-- <li class="faqBox"> -->
+                <!-- <img src="/sellPhone/common/img/imgFaq_3.png" class="backImg"> -->
+                <!-- <div class="infoGroup"> -->
+                    <!-- <div class="titleBox">왜 폰사요몰은</br>돈을 많이 주나요?</div> -->
+                    <!-- <a href="javascirpt:void(0);" class="tOnlyBtn toSpecSort">자세히보기</a> -->
+                    <!-- <img src="/sellPhone/common/img/underBtnLiner_1.png" class="underBtn"> -->
+                <!-- </div>  -->
+            <!-- </li> -->
+            <!-- <li class="faqBox"> -->
+                <!-- <img src="/sellPhone/common/img/imgFaq_4.png" class="backImg"> -->
+                <!-- <div class="infoGroup"> -->
+                    <!-- <div class="titleBox">내폰팔기</br>특급 이벤트?</div> -->
+                    <!-- <a href="javascirpt:void(0);" class="tOnlyBtn toSpecSort">자세히보기</a> -->
+                    <!-- <img src="/sellPhone/common/img/underBtnLiner_1.png" class="underBtn"> -->
+                <!-- </div>  -->
+            <!-- </li> -->
+        <!-- </ul> -->
+        <!-- <div class="mainCon listSort"> -->
+		    <!-- <div class="section listSort"> -->
+                <!-- <div class="listTopGroup"> -->
+                    <!-- <div class="tabBtnGroup"> -->
+                        <!-- <a href="javascript:void(0);" class="tOnlyBtn activated">애플</a> -->
+                        <!-- <a href="javascript:void(0);" class="tOnlyBtn">삼성</a> -->
+                        <!-- <a href="javascript:void(0);" class="tOnlyBtn">LG</a> -->
+                        <!-- <a href="javascript:void(0);" class="tOnlyBtn">폴더폰</a> -->
+                        <!-- <a href="javascript:void(0);" class="tOnlyBtn">태블릿</a> -->
+                        <!-- <a href="javascript:void(0);" class="tOnlyBtn">이벤트몰</a> -->
+                        <!-- <a href="javascript:void(0);" class="tOnlyBtn">구성품 및 악세사리</a> -->
+                    <!-- </div> -->
+                    <!-- <a href="javascript:void(0);" class="bgBtn filterSort">필터 설정</a> -->
+                <!-- </div> -->
+                <!-- <ul class="goodsList"> -->
+                    <!-- <li> -->
+                        <!-- <div class="gdImgBox"> -->
+                            <!-- <img src="/sellPhone/common/img/examplePhone_1.png"> -->
+                        <!-- </div> -->
+                        <!-- <div class="infoGroup"> -->
+                            <!-- <div class="gdNameBox">애플 아이폰8</div> -->
+                            <!-- <div class="prcBox">299,000</div> -->
+                        <!-- </div> -->
+                    <!-- </li> -->
+                    <!-- <li> -->
+                        <!-- <div class="gdImgBox"> -->
+                            <!-- <img src="/sellPhone/common/img/examplePhone_1.png"> -->
+                        <!-- </div> -->
+                        <!-- <div class="infoGroup"> -->
+                            <!-- <div class="gdNameBox">애플 아이폰8</div> -->
+                            <!-- <div class="prcBox">299,000</div> -->
+                        <!-- </div> -->
+                    <!-- </li> -->
+                    <!-- <li> -->
+                        <!-- <div class="gdImgBox"> -->
+                            <!-- <img src="/sellPhone/common/img/examplePhone_1.png"> -->
+                        <!-- </div> -->
+                        <!-- <div class="infoGroup"> -->
+                            <!-- <div class="gdNameBox">애플 아이폰8</div> -->
+                            <!-- <div class="prcBox">299,000</div> -->
+                        <!-- </div> -->
+                    <!-- </li> -->
+                    <!-- <li> -->
+                        <!-- <div class="gdImgBox"> -->
+                            <!-- <img src="/sellPhone/common/img/examplePhone_1.png"> -->
+                        <!-- </div> -->
+                        <!-- <div class="infoGroup"> -->
+                            <!-- <div class="gdNameBox">애플 아이폰8</div> -->
+                            <!-- <div class="prcBox">299,000</div> -->
+                        <!-- </div> -->
+                    <!-- </li> -->
+                    <!-- <li> -->
+                        <!-- <div class="gdImgBox"> -->
+                            <!-- <img src="/sellPhone/common/img/examplePhone_1.png"> -->
+                        <!-- </div> -->
+                        <!-- <div class="infoGroup"> -->
+                            <!-- <div class="gdNameBox">애플 아이폰8</div> -->
+                            <!-- <div class="prcBox">299,000</div> -->
+                        <!-- </div> -->
+                    <!-- </li> -->
+                    <!-- <li> -->
+                        <!-- <div class="gdImgBox"> -->
+                            <!-- <img src="/sellPhone/common/img/examplePhone_1.png"> -->
+                        <!-- </div> -->
+                        <!-- <div class="infoGroup"> -->
+                            <!-- <div class="gdNameBox">애플 아이폰8</div> -->
+                            <!-- <div class="prcBox">299,000</div> -->
+                        <!-- </div> -->
+                    <!-- </li> -->
+                <!-- </ul> -->
+                <!-- <div class="btnGroup moreShowSort"> -->
+                    <!-- <a href="javascript:void(0);" class="tOnlyBtn">더보기</a> -->
+                <!-- </div> -->
+            <!-- </div> -->
+        <!-- </div> -->
+
+
+
+
+        <a href="javascript:Void(0);" class="icBtn rtFloat"></a>
+    </div>
+
+
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/bottom.php';?>
+
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] .  '/sellPhone/common/pages/footer.php';?>
+
+
+</div>
+
+
+<!-- <script src="/intro/js/join.js"></script> -->
+
+
+</body>
+</html>
